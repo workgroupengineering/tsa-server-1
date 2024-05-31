455ff390f4d13ad3067e4ec7fad74029492be4b9
Daniel Meier <danielmeier50@gmail.com>
2024-05-31 17:32:06 +0200

    Update Dependencies
    * Update Quarkus to 3.11.0
    * Update Bouncy Castle to 1.78.1
    * Update AssertJ to 3.26.0
    * Update ArchUnit to 1.3.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ include("web")
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            version("quarkus", "3.9.1")
+            version("quarkus", "3.11.0")
             version("mapstruct", "1.5.5.Final")
 
             plugin("quarkus", "io.quarkus").versionRef("quarkus")
@@ -14,14 +14,14 @@ dependencyResolutionManagement {
             plugin("spotless", "com.diffplug.spotless").version("6.25.0")
 
             library("quarkus-bom", "io.quarkus", "quarkus-bom").versionRef("quarkus")
-            library("bouncycastle", "org.bouncycastle:bcpkix-jdk18on:1.77")
+            library("bouncycastle", "org.bouncycastle:bcpkix-jdk18on:1.78.1")
             library("mapstruct-processor", "org.mapstruct", "mapstruct-processor").versionRef("mapstruct")
             library("mapstruct-runtime", "org.mapstruct", "mapstruct").versionRef("mapstruct")
         }
 
         create("testLibs") {
-            library("assertj", "org.assertj:assertj-core:3.25.3")
-            library("archunit", "com.tngtech.archunit:archunit:1.2.1")
+            library("assertj", "org.assertj:assertj-core:3.26.0")
+            library("archunit", "com.tngtech.archunit:archunit:1.3.0")
         }
     }
 }
